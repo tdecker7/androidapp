5a8bec7a3b0b564ed79305813b9b199975861e5d
Trey Decker <treydecker75@gmail.com>
2019-04-03 18:33:40 -0600

    removed unused references

--- app/src/main/java/com/example/brewday/RecipeAdapter.kt
+++ app/src/main/java/com/example/brewday/RecipeAdapter.kt
@@ -2,8 +2,6 @@ package com.example.myapplication
 
 import android.content.Context
 import android.content.Intent
-import com.example.myapplication.R
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
